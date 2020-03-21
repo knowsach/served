f83624f5293f73d029f8fe471098a308781a04bb
knowsach <knowsach@gmail.com>
2020-03-21 21:56:07 +0530

    project folder dtructure setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Home } from './components/home-page/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: Home },
+  { path: '', component: Home }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import { Home } from './components/home-page/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Home
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
